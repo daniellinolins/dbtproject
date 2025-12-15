0065054e2bdf4306b6d1d3fd11e65e2bf7ef045c
Daniel Lins <dllaws@vohu.com.br>
2025-12-15 00:10:29 +0000

    add pre-hook lock table and begin transaction

--- models/bireport.sql
+++ models/bireport.sql
@@ -1,7 +1,11 @@
 {{
     config(
         materialized='table',        
+        pre_hook=['
+            BEGIN; LOCK TABLE {{target.schema}}.bireport;
+        '],     
         post_hook=['
+            COMMIT;
             GRANT USAGE ON SCHEMA {{target.schema}} TO GROUP biusers;
             GRANT SELECT ON TABLE {{target.schema}}.bireport to group biusers;
         ']
